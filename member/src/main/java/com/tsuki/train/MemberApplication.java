459b1768810981d911e0b6b755831084f32d81b0
juzz168 <391190087@qq.com>
2023-08-21 18:36:15 +0800

    增加http测试文件，方便测试

--- member/src/main/java/com/tsuki/train/MemberApplication.java
+++ member/src/main/java/com/tsuki/train/MemberApplication.java
@@ -12,6 +12,6 @@ public class MemberApplication {
         SpringApplication app = new SpringApplication(MemberApplication.class);
         Environment env = app.run(args).getEnvironment();
         log.info("启动成功！！");
-        log.info("地址：\thttp://127.0.0.1:{}", env.getProperty("server.port"));
+        log.info("地址：\thttp://127.0.0.1:{}{}/hello", env.getProperty("server.port"), env.getProperty("server.servlet.context-path"));
     }
 }
