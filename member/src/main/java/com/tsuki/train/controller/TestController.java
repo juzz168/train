d02adf9f5298597a45dd2fc0c44aca10fe8ee8b2
juzz168 <391190087@qq.com>
2023-08-21 17:40:30 +0800

    member

--- member/src/main/java/com/tsuki/train/controller/TestController.java
+++ member/src/main/java/com/tsuki/train/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.tsuki.train;
+package com.tsuki.train.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/hello")
     public String hello(){
-        return "Hello World!!!~~~~~~";
+        return "hello member";
     }
 }
