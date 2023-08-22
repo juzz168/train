c56e077dd652819cfe1e799d3e24041109f00d5f
juzz168 <391190087@qq.com>
2023-08-23 02:00:17 +0800

    使用Vue CLI  5创建web模块

--- member/src/main/java/com/tsuki/train/controller/MemberController.java
+++ member/src/main/java/com/tsuki/train/controller/MemberController.java
@@ -1,7 +1,11 @@
 package com.tsuki.train.controller;
 
+import com.tsuki.train.req.MemberRegisterReq;
+import com.tsuki.train.resp.CommonResp;
 import com.tsuki.train.service.MemberService;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +17,19 @@ public class MemberController {
     @Resource
     private MemberService memberService;
     @GetMapping("/count")
-    public Integer count(){
-        return memberService.count();
+    public CommonResp<Integer> count(){
+        int count = memberService.count();
+        CommonResp<Integer> commonResp = new CommonResp<>();
+        commonResp.setContent(count);
+        return commonResp;
+    }
+
+    @PostMapping("/register")
+    public CommonResp<Long> register(@Valid MemberRegisterReq req){
+        long register = memberService.register(req);
+       /* CommonResp<Long> commonResp = new CommonResp<>();
+        commonResp.setContent(register);
+        return commonResp;*/
+        return new CommonResp<>(register);
     }
 }
--- member/src/main/java/com/tsuki/train/service/MemberService.java
+++ member/src/main/java/com/tsuki/train/service/MemberService.java
@@ -1,9 +1,17 @@
 package com.tsuki.train.service;
 
+import cn.hutool.core.collection.CollUtil;
+import com.tsuki.train.domain.Member;
+import com.tsuki.train.domain.MemberExample;
+import com.tsuki.train.exception.BusinessException;
+import com.tsuki.train.exception.BusinessExceptionEnum;
 import com.tsuki.train.mapper.MemberMapper;
+import com.tsuki.train.req.MemberRegisterReq;
+import com.tsuki.train.util.SnowUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @Service
 public class MemberService {
@@ -11,5 +19,20 @@ public class MemberService {
     private MemberMapper memberMapper;
     public int count() {
         return Math.toIntExact(memberMapper.countByExample(null));
+    }
+
+    public long register(MemberRegisterReq req){
+        String mobile = req.getMobile();
+        MemberExample memberExample = new MemberExample();
+        memberExample.createCriteria().andMobileEqualTo(mobile);
+        List<Member> list = memberMapper.selectByExample(memberExample);
+        if (CollUtil.isNotEmpty(list)){
+            throw  new BusinessException(BusinessExceptionEnum.MEMBER_MOBILE_EXIST);
+        }
+        Member member = new Member();
+        member.setId(SnowUtil.getSnowflakeNextId());
+        member.setMobile(mobile);
+        memberMapper.insert(member);
+        return member.getId();
     }
 }
