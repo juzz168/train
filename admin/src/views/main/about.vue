d3c6721809d395a5f277b0ba853011673518fa54
juzz168 <391190087@qq.com>
2023-08-28 02:15:01 +0800

    前端admin

---
+++ admin/src/views/main/about.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1>关于12303</h1>
+</template>
+<script setup>
+</script>
---
+++ admin/src/views/main/welcome.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>欢迎使用甲蛙12306售票系统</h1>
+</template>
+<script setup>
+</script>
+<style>
+</style>
